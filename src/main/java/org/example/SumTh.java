ca679916b8e61f7614e1954b1e6edd702bac8b9e
YDH-CEC <dubsteb@naver.com>
2023-09-11 16:33:46 +0900

    SumThread HW is done. The sum from 1 to 100 is calculated in two threads and added in main.

--- src/main/java/org/example/SumTh.java
+++ src/main/java/org/example/SumTh.java
@@ -2,50 +2,50 @@ package org.example;
 
 public class SumTh {
     public static void main(String[] args) {
-        int[] x = new int[100];
+        int[] x = new int[100]; // 배열 생성
 
         for (int i = 0; i < 100; i++){
-            x[i] = i;
+            x[i] = i; // 배열에 0~99 저장
         }
 
-        int[] arr1= new int[50];
-        int[] arr2= new int[50];
+        int[] arr1= new int[50]; // 배열 1
+        int[] arr2= new int[50]; // 배열 2
         System.arraycopy(x, 0, arr1, 0, 50); // 배열을 잘라내기 위한 arraycopy()
-        System.arraycopy(x, 50, arr2, 0, 50);
+        System.arraycopy(x, 50, arr2, 0, 50); // 두 부분 0~49 50~99로 나눔
 
-        Thread th1 = new Thread(new SumThread(arr1));
-        Thread th2 = new Thread(new SumThread(arr2));
+        SumThread th1 = new SumThread(arr1); // 스레드 1
+        SumThread th2 = new SumThread(arr2); // 스레드 2
 
-        th1.start();
-        th2.start();
+        th1.start(); // 스레드 1 시작
+        th2.start(); // 스레드 2 시작
 
-        try {
-            th1.join();
+        try { // join이 제대로 수행됐는지 확인하기 위한 try-catch문
+            th1.join(); // 스레드 1
             th2.join();
         } catch (InterruptedException e){
             e.printStackTrace();
         }
 
-        int result = th1.getSumResult() + th2.getSumResult();
+        int result = th1.getResult() + th2.getResult(); // 두 스레드의 계산값을 더함
         System.out.println("최종 결과 : " + result);
     }
 }
 
-class SumThread implements Runnable {
+class SumThread extends Thread {
     private int[] array;
     private int result;
 
     public SumThread(int[] array) {
         this.array = array;
-    }
+    } // array를 받아 저장
 
     @Override
     public void run() {
         for(int num : array) {
             result += num;
         }
-    }
-    public int getSumResult(){
+    } // 스레드는 배열을 받아 값들을 더해 result에 저장
+    public int getResult(){
         return result;
-    }
+    } // 결과를 얻기 위한 getResult()
 }
